10fabc6ee43b0f501d90ff989aaaf5f68e25d15c
kingsedu <chinedueh26@yahoo.com>
2025-12-04 11:13:33 +0100

    added script to the parentFolder

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,10 +1,24 @@
 import express from "express";
-
+import path from "node:path";
+import { ENV } from "./configs/env.js";
 const app = express();
-
+const __dirname = path.resolve();
+console.log(path.join(__dirname, "../admin"));
+console.log(path.join(__dirname, "../admin", "dist", "index.html"));
 app.get("/api/health", (req, res) => {
   res.status(200).json({ message: "succefull" });
 });
-app.listen(3000, () => {
-  console.log(`server is running 123 abc`);
+
+// make our app ready for deployment
+if (ENV.NODE_ENV === "production") {
+  const staticPath = path.join(__dirname, "../admin/dist");
+  app.use(express.static(staticPath));
+
+  app.get("/{*any}", (req, res) => {
+    res.sendFile(path.join(staticPath, "index.html"));
+  });
+}
+
+app.listen(ENV.PORT, () => {
+  console.log(`server is running http://localhost:${ENV.PORT}`);
 });
