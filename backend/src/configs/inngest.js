c452195159f6bcf10c753e76767025125a64bcbd
kingsedu <chinedueh26@yahoo.com>
2025-12-04 14:08:54 +0100

    inngest set-up

--- backend/src/configs/inngest.js
+++ backend/src/configs/inngest.js
@@ -1,9 +1,42 @@
-import { v2 as cloudinary } from "cloudinary";
+import { Inngest } from "inngest";
 import { ENV } from "./env.js";
-cloudinary.config({
-  cloud_name: ENV.CLOUDINARY_NAME,
-  api_key: ENV.CLOUDINARY_API_KEY,
-  api_secret: ENV.CLOUDINARY_API_SECRECT,
-});
+import { connectDataBase } from "../db/db.js";
+import { User } from "../models/user.schema.js";
+export const inngest = new Inngest({ id: "ecommerce-app" });
 
-export default cloudinary;
+const syncUser = inngest.createFunction(
+  { id: "sync-user" },
+  { event: "clerk/user.created" },
+  async ({ event }) => {
+    try {
+      await connectDataBase(ENV.DB_URL);
+      const { id, email_addresses, first_name, last_name, image_url } =
+        event.data;
+
+      const newUser = {
+        clerkId: id,
+        email: email_addresses?.[0]?.email_address,
+        name: `${first_name || ""} ${last_name || ""}`.trim() || "User",
+        imageUrl: image_url,
+        addresses: [],
+        wishlist: [],
+      };
+
+      await User.create(newUser);
+    } catch (error) {
+      console.error("Failed to sync user:", error);
+      throw error;
+    }
+  }
+);
+
+const deleteUser = inngest.createFunction(
+  { id: "delete-user-from-db" },
+  { event: "clerk/user.deleted" },
+  async ({ event }) => {
+    await connectDataBase(ENV.DB_URL);
+    const { id } = event.data;
+    await User.deleteOne({ clerkId: id });
+  }
+);
+export const functions = [syncUser, deleteUser];
